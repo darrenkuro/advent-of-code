ae2f1e73e1858297b1090ebd4604567913d07aa6
darrenkuro <odon5ht@gmail.com>
2023-12-02 18:49:28 +0100

    2023 day2 crappy

---
+++ 2023/day2/p1.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "input");
+const fileContent = fs.readFileSync(filePath, "utf8");
+
+result = 0;
+
+fileContent.split("\n").forEach((line) => {
+    if (!line) return;
+    const id = +/Game (\d+)/.exec(line)[1];
+    let possible = true;
+    line.match(/(\d+) green/g).forEach((val) => {
+        if (+val.split(" ")[0] > 13) possible = false;
+    });
+    line.match(/(\d+) blue/g).forEach((val) => {
+        if (+val.split(" ")[0] > 14) possible = false;
+    });
+    line.match(/(\d+) red/g).forEach((val) => {
+        if (+val.split(" ")[0] > 12) possible = false;
+    });
+
+    if (!possible) return;
+    result += id;
+});
+
+console.log(result);
+
+// part2
+result = 0;
+
+fileContent.split("\n").forEach((line) => {
+    if (!line) return;
+    // const id = +/Game (\d+)/.exec(line)[1];
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+    line.match(/(\d+) green/g).forEach((val) => {
+        green = +val.split(" ")[0] > green ? +val.split(" ")[0] : green;
+    });
+    line.match(/(\d+) blue/g).forEach((val) => {
+        blue = +val.split(" ")[0] > blue ? +val.split(" ")[0] : blue;
+    });
+    line.match(/(\d+) red/g).forEach((val) => {
+        red = +val.split(" ")[0] > red ? +val.split(" ")[0] : red;
+    });
+
+    result += red * blue * green;
+});
+
+console.log(result);
